8da4867b44c7ddb7da93efe8435ccf868efacda6
markpassando <markpassando@gmail.com>
2017-06-13 21:16:30 -0400

    w7d3

---
+++ w7d3/skeleton/util/api_util.js
@@ -0,0 +1,6 @@
+export const fetchSearchGiphys = (query) => {
+  return $.ajax({
+    method: 'GET',
+    url: `http://api.giphy.com/v1/gifs/search?q=${query}&api_key=dc6zaTOxFJmzC&limit=2`
+  });
+};
